b77c39b65724f9a6c83f54f38cdcfe6f26ee202b
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-11 13:50:03 +0300

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main()
+{
+	int a = 97;
+	while(a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 65;
+	while(a < 91)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
