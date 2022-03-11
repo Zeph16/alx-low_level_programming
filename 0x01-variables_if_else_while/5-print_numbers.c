aee853434ee95244a7f83a31862277e2f0308af0
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-11 14:02:36 +0300

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main()
+{
+	char a = '0';
+	putchar(a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar(++a);
+	putchar('\n');
+	return 0;
+}
