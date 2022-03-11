290ee4c5608f82474a247730d7dcd5113adb7347
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-11 15:59:48 +0300

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,15 @@ int main(void)
 {
 	int a = 48;
 
-	putchar(a);
-	a++;
 	do {
-		putchar(',');
-		putchar(' ');
 		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		a++;
 	} while (a != 58);
+	putchar('\n');
 	return (0);
 }
