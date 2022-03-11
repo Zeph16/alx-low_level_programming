4da938b1be389a97cd7bacecd5815ee291ad6188
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-11 13:47:51 +0300

    Task 2

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main()
+{
+	int a = 97;
+	while(a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
