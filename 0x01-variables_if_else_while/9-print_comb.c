27a06685991e43ec55a871758937f6173f4a53f7
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-11 15:56:19 +0300

    Task 8 test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	putchar(a);
+	a++;
+	do {
+		putchar(',');
+		putchar(' ');
+		putchar(a);
+		a++;
+	} while (a != 58);
+	putchar('\n');
+	return (0);
+}
