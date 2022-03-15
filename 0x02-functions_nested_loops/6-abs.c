d55654e3839eb1216a4e4a98342b00abfc0a5a96
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 02:39:26 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of a number
+ * @n: number to be absoluted...yes that's a word
+ *
+ * Return: Absolute value of the number
+ */
+
+int _abs(int n)
+{
+	return (n * -1);
+}
