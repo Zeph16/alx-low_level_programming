74d32906f77f269bbd7b52c7c14e39f1526e0aa3
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 02:53:13 +0300

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - ...prints the last digit
+ * @n: Number to be processes
+ *
+ * Return: Last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
