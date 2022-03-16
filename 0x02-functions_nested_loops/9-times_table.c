df974f554d676e14d980549d48a73f9fa0de3509
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 04:28:08 +0300

    Task 9 again again

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 				_putchar(prod / 10 + '0');
 				_putchar(prod % 10 + '0');
 			}
-			else
+			else if (prod < 10 && prod != 0)
 			{
 				_putchar(' ');
 				_putchar(prod + '0');
