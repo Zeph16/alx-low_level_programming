5802b66bdaa305e2c353e91012c9c9332587e05c
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 03:59:33 +0300

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int first = 48;
+	int second = 48;
+	int third = 48;
+	int fourth = 48;
+
+	while (second != 52 || first != 50)
+	{
+		_putchar(first);
+		_putchar(second);
+		_putchar(':');
+		_putchar(third);
+		_putchar(fourth);
+		_putchar('\n');
+		fourth++;
+		if (fourth > 57)
+		{
+			fourth = 48;
+			third++;
+		}
+		if (third > 53)
+		{
+			third = 48;
+			fourth = 48;
+			second++;
+		}
+		else if (second > 57)
+		{
+			second = 48;
+			first++;
+		}
+	}
+}
