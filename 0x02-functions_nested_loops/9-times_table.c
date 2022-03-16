4d8ee51a554ae3554225cbbcbbe719795a4f9139
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 04:22:27 +0300

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int prod;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = i * j;
+			if (prod >= 10)
+			{
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			else
+			{
+				_putchar(prod + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
