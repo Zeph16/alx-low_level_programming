2ff0a1ea72104629856dbfd17fed0a01649675d7
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 04:47:09 +0300

    Task 11:

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - natural numbers from n to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	int cur;
+
+	for (cur = n; cur != 98; cur < 98 ? i++:i--)
+	{
+		printf("%i, ", cur);
+	}
+	printf("98\n");
+	return (0);
+}
