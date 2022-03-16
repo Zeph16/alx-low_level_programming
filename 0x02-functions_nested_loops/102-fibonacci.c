7e81fa5c617722b3866fc01dffbf6d2a269e797f
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-16 05:58:44 +0300

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Fibonacci
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	long prev = 1;
+	long cur = 1;
+	long temp;
+
+	printf("1, 1, ");
+	for (i = 2; i < 50; i++)
+	{
+		temp = prev + cur;
+		printf("%ld", temp);
+		prev = cur;
+		cur = temp;
+		if (i != 49)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
