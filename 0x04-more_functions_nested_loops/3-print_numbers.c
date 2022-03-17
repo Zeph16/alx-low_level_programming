1571dd94f347d5dbcf5d31dbee22d8d8da6fe77e
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-18 00:10:50 +0300

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - ...prints numbers
+ *
+ */
+
+void print_numbers(void)
+{
+	int a = 48;
+
+	do {
+		putchar(a);
+		a++;
+	} while (a != 58);
+	putchar('\n');
+}
