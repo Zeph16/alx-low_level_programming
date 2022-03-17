41fa74e6c1229820a23448c27e2c600738426b25
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-18 00:52:12 +0300

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14
+ */
+
+void more_numbers(void)
+{
+	int i = 48;
+	int j = 0;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		while (j < 53)
+		{
+			_putchar(i);
+			if (j > 47)
+				_putchar(j++);
+			if (i == 58)
+			{
+				i = 49;
+				j = 48;
+			}
+			if (j == 0)
+				i++;
+		}
+		_putchar('\n');
+		i = 48;
+		j = 0;
+	}
+}
+
