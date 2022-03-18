1c9cb9c6a8fc0b2412a2806378c91dfef7b83a3d
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-18 04:41:33 +0300

    Task 12 again

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,46 @@
 #include "main.h"
 
 /**
- * print_number - prints a number using putchar
- * @n - number to be printed
+ * print_number - prints number
+ *
+ * @n: integer to print to character
  */
-
 void print_number(int n)
 {
-	int dup, dup2, i;
-	int counter = 0;
-	int multiplier = 1;
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
 
 	if (n == 0)
+	{
 		_putchar('0');
-	else if (n < 0)
+	}
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -n - 1;
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
 	}
-	dup = n + 1;
-	dup2 = n + 1;
 
-	while (dup != 0)
+	while (x != 0)
 	{
-		dup /= 10;
-		counter++;
+		x = x / 10;
+		c++;
 	}
-	for (i = 0; i < counter - 1; i++)
+	for (i = 1; i < c; i++)
 	{
-		multiplier *= 10;
+		d *= 10;
 	}
 	for (i = 0; i < c; i++)
 	{
-		_putchar(dup2 / multiplier + '0');
-		dup2 %= multiplier;
-		multiplier /= 10;
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
 	}
 }
