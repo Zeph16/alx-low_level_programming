e888a46008f5367d6a5972eb46327c89e00c5e99
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-21 08:23:29 +0300

    Directory setup

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Assigns 98 to an int variable
+ *
+ * *n: address of int variable
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Sixth C project - pointers
