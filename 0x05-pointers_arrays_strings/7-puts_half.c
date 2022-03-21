56b18da79ad5494f89096926dcf82514dcc5a3e6
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-21 10:54:40 +0300

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: processed string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 != 0)
+		len -= 1;
+	for (i = len / 2; *(str + i) != '\0'; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
