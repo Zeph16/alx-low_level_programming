0531bf417b4041c09377223e5b9b60212612b496
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-21 18:39:08 +0300

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 {
 	int i, j, len, index, multiplier = 1, converted = 0,  nums = 0;
 	int plus = 0, minus = 0;
-	int done = 0, start = 0, end = 0;
+	int done = 0, start = 1000, end = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -25,14 +25,14 @@ int _atoi(char *s)
 			end = i;
 		if (nums > 0)
 		{
-			if (start == 0)
+			if (start == 1000)
 				start = i;
 			done = 1;
 		}
 	}
 	if (end == 0)
 		end = i;
-	if (start == 1)
+	if (start == 1000)
 		start = 0;
 	len = end - start;
 	index = len - 1;
