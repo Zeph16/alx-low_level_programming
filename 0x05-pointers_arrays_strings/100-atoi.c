7db0c8ad0eaa60f8b6a5841ec8523b1358d9dd0a
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-21 18:16:53 +0300

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,46 +9,40 @@
 
 int _atoi(char *s)
 {
-	int i, j, len, plus = 0, minus = 0, multiplier = 1, converted = 0, index = 0, nums = 0;
-	int done = 0;
-	char *strnum;
+	int i, j, len, index, multiplier = 1, converted = 0,  nums = 0;
+	int plus = 0, minus = 0;
+	int done = 0, start = 0, end = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 48 && s[i] <= 57 && !done)
-		{
-			strnum[nums] = s[i];
 			nums++;
-		}
 		else if (s[i] == 43 && !done)
-		{
 			plus++;
-		}
 		else if (s[i] == 45 && !done)
-		{
 			minus++;
-		}
-		else if (nums > 0)
+		if (done && !(s[i] >= 48 && s[i] <= 57) && end == 0)
+			end = i;
+		if (nums > 0)
 		{
+			if (start == 0)
+				start = i;
 			done = 1;
 		}
 	}
-	len = nums;
-	index = nums;
-	for (i = 0; i < len; i++)
+	len = end - start;
+	index = len - 1;
+	for (i = start; i < end; i++)
 	{
-		for (j = 0; j < index - 1; index--)
+		for (j = i - start; j < index; index--)
 		{
 			multiplier *= 10;
 		}
-		converted += (strnum[i] - 48) * multiplier;
+		converted += (s[i] - 48) * multiplier;
 		multiplier = 1;
-		nums--;
+		index = len - 1;
 	}
 	if (minus > plus)
 		converted *= -1;
-	
 	return (converted);
 }
-
-		
