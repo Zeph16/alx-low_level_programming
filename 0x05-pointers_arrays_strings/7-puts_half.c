bbe4aa868d33ef80a33425dc23789134216a1c18
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-21 11:49:59 +0300

    Task 7 again again

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,7 @@
 
 void puts_half(char *str)
 {
-	int i;
+	int i, start;
 	int len = 0;
 
 	for (i = 0; *(str + i) != '\0'; i++)
@@ -16,8 +16,10 @@ void puts_half(char *str)
 	}
 
 	if (len % 2 != 0)
-		len -= 1;
-	for (i = len / 2 + 1; *(str + i) != '\0'; i++)
+		start = (len - 1) / 2;
+	else
+		start = len / 2;
+	for (i = start; *(str + i) != '\0'; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 }
