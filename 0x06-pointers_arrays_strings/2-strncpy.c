2fcd1c3a834f8eedf92f44e3f54f0035a1ad3094
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-25 13:01:24 +0300

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
 	int i;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
-	dest[i] = '\0';
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
