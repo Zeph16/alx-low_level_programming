e923f7ab0dd75b335a6fd1733399ca82ef6d7fe6
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-26 01:15:33 +0300

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes string to uppercase
+ * @s: string to be changed
+ *
+ * Return: char pointer to changed string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] += 32;
+	return (s);
+}
+
