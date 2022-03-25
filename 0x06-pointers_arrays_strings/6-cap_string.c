015e3944e08caf2028952a9d75c67ac547e55f8b
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-26 01:46:03 +0300

    Task 6 aaaaaa

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 
 char *cap_string(char *s)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -22,7 +22,6 @@ char *cap_string(char *s)
 		if (s[i] == ')' || s[i] == '{' || s[i] == '}')
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] -= 32;
-
 	}
 
 	return (s);
