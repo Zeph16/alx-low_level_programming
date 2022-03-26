86684b01dd15a6f3807b856376c57afc14e1c37e
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-26 03:15:00 +0300

    Task 6 done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes the first letter of all words in a string
- * @s: the string
+ * cap_string - capitalizes the first letter of all words in the string
+ * @s: string
  *
- * Return: char pointer to processed string
+ * Return: char pointer to the updated string
  */
 
 char *cap_string(char *s)
 {
 	int i = 0;
 
-
 	if (s[i] >= 97 && s[i] <= 122)
-		s[i + 1] -= 32;
-	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == '\n' || s[i] == ',' || s[i] == '.'
-			|| s[i] == '!' || s[i] == '"' || s[i] == '(' || s[i] == '{'
-			|| s[i] == '\t' || s[i] == ')' || s[i] == '}' || s[i] == ';'
-			|| s[i] == '?')
-		{
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '!' || s[i] == '?' || s[i] == '"'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
-			{
 				s[i + 1] -= 32;
-			}
-		}
+		i++;
 	}
 	return (s);
 }
