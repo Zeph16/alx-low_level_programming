5fbcedbc144f62231c8ef0f3df5d1e865f872289
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-28 23:33:14 +0300

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: char pointer to the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *loc;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			*loc = c;
+
+	return (loc);
+}
