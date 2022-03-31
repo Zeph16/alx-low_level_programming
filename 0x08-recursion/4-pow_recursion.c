01b7ddaa38ccf83cd5d9fd9adb54d93045e3862c
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-31 15:00:55 +0300

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x raised to y via recursion
+ * @x: x
+ * @y: y
+ *
+ * Return: x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y != 0)
+		return _pow_recursion(x, --y) * x;
+	else
+		return (1);
+}
