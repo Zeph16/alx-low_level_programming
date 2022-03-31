b02ed22f33eb2c69f48f638a3eb4b36ee9c0c121
Sofonias Ayele <sof16ayele@gmail.com>
2022-03-31 14:49:39 +0300

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string via recursion
+ * @s: the string
+ *
+ * Return: the length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+		return (_strlen_recursion(s + 1) + 1);
+	else
+		return (0);
+}
