2c0efc7816a3f3a7808cedaf11312cc4a692a104
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-05 20:34:06 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,8 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] == '\0'; i++)
 		;
-	i--;
 	for (j = 0; s2[j] == '\0'; j++)
 		;
-	i--;
 	arr = (char *)malloc(sizeof(char) * (i + j));
 	if (arr == NULL)
 		return (arr);
