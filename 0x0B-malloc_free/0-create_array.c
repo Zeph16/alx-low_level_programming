f49da6b35d216a3e32ce99dfae873dd7bb2bc794
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-05 20:00:39 +0300

    Task 0 done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr = NULL;
 	unsigned int i;
-
+	
+	if (size == 0)
+		return (arr);
 	arr = malloc(sizeof(c) * size);
-	if (arr == NULL || size == 0)
+	if (arr == NULL)
 		return (arr);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
