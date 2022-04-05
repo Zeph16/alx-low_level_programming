245db2a04dc40a524d1d85ed480a0045375c813b
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-05 20:38:26 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,11 +22,12 @@ char *str_concat(char *s1, char *s2)
 		;
 	for (j = 0; s2[j] == '\0'; j++)
 		;
-	arr = (char *)malloc(sizeof(char) * (i + j));
+	arr = (char *)malloc(sizeof(char) * (i + j + 1));
 	if (arr == NULL)
 		return (arr);
 	for (i = 0; s1[i] == '\0'; i++)
 		arr[i] = s1[i];
+	i--;
 	for (j = 0; s2[j] == '\0'; j++)
 		arr[i + j] = s2[j];
 	return (arr);
