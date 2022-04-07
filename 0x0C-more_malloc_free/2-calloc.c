97ac058b131fbc6104e0d2a8a946c8e7fd321f13
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-07 21:56:47 +0300

    Task 2 again

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	char *arr;
 
 	if (nmemb == 0 || size == 0)
@@ -21,6 +22,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(arr);
 		return (NULL);
 	}
+
+	for (i = 0; i < (nmemb * size); i++)
+		*(p + i) = 0;
 
 	return (arr);
 }
