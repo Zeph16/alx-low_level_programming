9edd65fd07be525be97df6279a3794f50ecb67bd
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-11 22:33:06 +0300

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog variable
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ * Return: a dog variable
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doge;
+
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+		return (NULL);
+	doge->name = name;
+	doge->age = age;
+	doge->owner = owner;
+	return (doge);
+}
