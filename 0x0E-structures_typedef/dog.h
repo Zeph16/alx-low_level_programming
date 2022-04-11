3402ac01aebbbc4db17cc1a593b2f246caa1eec0
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-11 21:46:29 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOGE
+#define DOGE
+
+/**
+ * struct dog - ...it's a dog structure with a name, an age and an owner
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
