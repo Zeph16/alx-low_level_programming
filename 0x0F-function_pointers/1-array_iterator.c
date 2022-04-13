c781669132b7b195e365947b8272bf5d73c20646
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-13 21:47:07 +0300

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints the elements of an array via a provided function
+ * @array: the array
+ * @size: the size of the array
+ * @action: the provided function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
