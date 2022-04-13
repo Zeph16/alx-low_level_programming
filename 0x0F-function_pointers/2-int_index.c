d81ba8e467f18b33952d95b1215d9762338e9e79
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-13 22:01:22 +0300

    Task 2 done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	size_t i;
+	int i;
 
 	if (size <= 0)
 		return (-1);
 	if (array != NULL)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]) != 0)
-				return (0);
+				return (i);
 	return (-1);
 }
