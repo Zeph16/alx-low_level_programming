6af14a911f68d26d608e002aaf105b0e83a2b4ba
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-14 19:24:12 +0300

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints a certain amount of numbers
+ * @separator: character between numbers
+ * @n: the certain amount
+ * @...: the numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator)
+			printf("%d%s", va_arg(nums, int), separator);
+		else
+			printf("%d", va_arg(nums, int));
+	}
+	printf("%d\n", va_arg(nums, int));
+}
