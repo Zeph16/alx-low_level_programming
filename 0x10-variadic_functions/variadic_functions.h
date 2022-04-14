c80d2de0fb1bcfae6b7a7f8f2c908e6c243c5e00
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-15 02:35:24 +0300

    Ran out of time

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
 #ifndef var_fun
 #define var_fun
 
-#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
