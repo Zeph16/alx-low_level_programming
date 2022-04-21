8f1a309708216705b53ad5577e4118374e96d9f0
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-21 18:01:41 +0300

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,11 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 1;
-	list_t *temp = h->next;
+	list_t *temp;
 
 	if (!h)
 		return (0);
+	temp = h->next;
 
 	if (h->str)
 		printf("[%d] %s\n", h->len, h->str);
