db3ab1483f85577e677caf7ca978f4eaf5deb524
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-21 19:51:47 +0300

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,6 @@ void premain(void) __attribute__((constructor));
  */
 void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
-
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
