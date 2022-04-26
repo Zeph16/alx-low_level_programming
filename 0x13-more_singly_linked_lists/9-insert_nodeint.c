2e31e4a94b7fbbe3e9a00037fa804c7e12efc504
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-27 01:26:55 +0300

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!node)
 		return (NULL);
 	node->n = n;
+	if (idx == 0)
+	{
+		node->next = (*head)->next;
+		*head = node;
+		return (node);
+	}
 	node->next = temp->next;
 	temp->next = node;
 	return (node);
