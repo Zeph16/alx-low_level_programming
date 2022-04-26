d7b1e1e52b08d8fc612f50dc37a7ae2b44ff803c
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-26 21:30:49 +0300

    Task 2 and 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  * Return: address of the new node
  */
 
-listint_t *add_nodeint_end(list_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *last = *head;
 
