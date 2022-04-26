c1cf1f754dc808ea8f0a71434dcde7f609096f27
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-27 00:13:19 +0300

    Task 5

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the first entry of a list and returns its value
+ * @head: pointer to the pointer to the first entry
+ *
+ * Return: the value
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (!head)
+		return 0;
+	n = (*head)->n;
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+	return n;
+}
