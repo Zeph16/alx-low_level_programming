54238919d6d3a784f6e13c2116e24060bfa81c2c
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-26 23:54:10 +0300

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,21 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (!*head)
-	{
-		*head = NULL;
 		return;
-	}
 	while (temp)
 	{
 		temp = (*head)->next;
-		if (temp)
-		{
-			free(*head);
-			*head = temp;
-		}
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
