c896fa5801bd9507578c9f2d3b1feee1c0fc86fe
Sofonias Ayele <sof16ayele@gmail.com>
2022-04-29 00:44:56 +0300

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the bit of a number at a given index
+ * @n: the number
+ * @index: the index
+ *
+ * Return: the bit or -1 on failure
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int shifter = 1, checker, i;
+
+	for (i = 0; i < index; i++)
+		shifter <<= 1;
+	if (shifter > n)
+		return (-1);
+	checker = shifter & n;
+	if (checker == shifter)
+		return (1);
+	return (0);
+}
