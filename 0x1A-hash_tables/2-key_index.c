62a58d4a454fe17494aad936f6f35b352d5f6572
Sofonias Ayele <sof16ayele@gmail.com>
2022-07-01 22:49:48 +0300

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,6 +14,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	if (size == 0 || key == NULL)
 		return (0);
 	index = hash_djb2(key) % size;
-	
-	return index;
+
+	return (index);
 }
