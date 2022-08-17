9d0d3f5e410c244ff2862b205bb6dca7f8c40328
Sofonias Ayele <sof16ayele@gmail.com>
2022-08-17 21:43:57 +0300

    Tasks 4 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Task 5 of the Makefile project """
+
+
+def island_perimeter(grid):
+    """ Algorithm that calculates the perimeter of the island """
+    perimeter = 0
+
+    nrows = len(grid)
+
+    if grid != []:
+        ncolumns = len(grid[0])
+
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
